5faefd923c01c1b21909c540b315dad966720ce3
lysom <lysom@outlook.com>
2018-04-18 14:25:10 +0800

    this is a test 4

---
+++ src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="wrap">
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .wrap{ width:100%; height:100%; background:url(/static/images/bg.jpg) no-repeat center center/cover; }
+</style>
