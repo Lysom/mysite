45120801642cc503d77649e414e7dea39c8f24d3
Lysom <Lysom.Yu@outlook.com>
2018-05-23 13:26:47 +0800

    修改音乐

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <audio src="/static/music/planet.mp3" autoplay="autoplay" loop="loop" preload="auto"  @play="greet">
+    <audio id="audio1" src="/static/music/planet.mp3" autoplay="autoplay" loop="loop" preload="auto"  @play="greet">
       Your browser does not support the audio tag.
     </audio>
-    <div class="bgMusic"></div>
+    <div class="bgMusic" :class="{play:isPlayed}" @click="togglePlayState"></div>
     <router-view/>
   </div>
 </template>
@@ -13,12 +13,24 @@ export default {
   name: 'App',
   data () {
     return {
-
+      isPlayed: false,
+      isPaused: false,
+      audioObj: null
     }
+  },
+  updated () {
+    this.audioObj = document.querySelector('#audio1')
   },
   methods: {
     greet: function (event) {
       event.target.volume = 0.01
+      this.isPlayed = true
+    },
+    togglePlayState: function () {
+      this.isPlayed = !this.isPlayed
+      this.isPaused = !this.isPaused
+      // console.log(this.audioObj)
+      this.isPaused ? this.audioObj.pause() : this.audioObj.play()
     }
   }
 }
@@ -34,9 +46,8 @@ html,body{ width:100%; height:100%; }
   color: #2c3e50;
   height:100%;
   /* 背景音乐 */
-  .bgMusic{ position:fixed; top:30px; right:30px; width:75px; height:75px; border-radius: 100%; background:url(/static/images/msign.png) no-repeat center center/cover pink; background-size:90% auto;
-    animation: rotateM 3s linear infinite;
-  }
+  .bgMusic { position:fixed; top:30px; right:30px; width:75px; height:75px; border-radius: 100%; background:url(/static/images/msign.png) no-repeat center center/cover pink; background-size:90% auto;}
+  .play { animation: rotateM 3s linear infinite; }
   @keyframes rotateM {
     0% { transform: rotateZ(0) }
     100% { transform: rotateZ(-360deg) }
